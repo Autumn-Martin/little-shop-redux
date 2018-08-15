91bdc27170697dcc61874c0c90076fa918bda460
Tim Fielder <fiel1139@gmail.com>
2018-08-15 17:04:14 -0600

    added Validations tests to merchant_spec.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,12 @@
 RSpec.describe Merchant do
   describe 'Validations' do
+    it 'is invalid without a name' do
+      merchant_1 = Merchant.create(name: nil)
+      expect(merchant_1).to_not be_valid
+
+      merchant_2 = Merchant.create(name: "Whatever")
+      expect(merchant_2).to be_valid
+    end
     it 'has many items' do
       association = described_class.reflect_on_association(:items)
       expect(association.macro).to eq :has_many
