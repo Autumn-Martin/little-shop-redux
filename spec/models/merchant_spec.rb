ec4924bec6fef5eccdeab72799f7189f05fab743
Tim Fielder <fiel1139@gmail.com>
2018-08-15 17:03:58 -0600

    added to merchant_spec.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe Merchant do
   describe 'Validations' do
-
-    
-
+    it 'has many items' do
+      association = described_class.reflect_on_association(:items)
+      expect(association.macro).to eq :has_many
+    end
   end
 end
