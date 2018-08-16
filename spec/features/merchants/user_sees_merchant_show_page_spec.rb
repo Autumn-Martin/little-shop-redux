2bcd617f5b2e317631d52f6048d645e86fa86cd1
Tim Fielder <fiel1139@gmail.com>
2018-08-16 13:51:15 -0600

    adds user sees show page tests.

--- spec/features/merchants/user_sees_merchant_show_page_spec.rb
+++ spec/features/merchants/user_sees_merchant_show_page_spec.rb
@@ -1,14 +1,14 @@
-RSpec.describe 'Merchant index page' do
+RSpec.describe 'Merchant show page' do
   before(:each) do
       @merchant_1 = Merchant.create(name: "Bilbo")
       @merchant_2 = Merchant.create(name: "Ikea")
   end
   context 'as a visitor' do
-    it 'shows list of merchants' do
-      visit '/merchants'
+    it 'shows an individual merchant' do
+      visit "/merchants/#{@merchant_1.id}"
 
       expect(page).to have_content(@merchant_1.name)
-      expect(page).to have_content(@merchant_2.name)
+      expect(page).to have_content("ID: #{@merchant_1.id}")
     end
 
 
