29d2e3ab64e9acf5cedeb0a4b66efcc96738782f
Tim Fielder <fiel1139@gmail.com>
2018-08-20 17:27:16 -0600

    fixes migration from testing failure for invoice_items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,6 +48,8 @@ CSV.foreach('./data/invoice_items.csv', headers: true, header_converters: :symbo
                     id:               invoice_item[:id],
                     item_id:          invoice_item[:item_id],
                     invoice_id:       invoice_item[:invoice_id],
+                    quantity:         invoice_item[:quantity],
+                    unit_price:       invoice_item[:unit_price],
                     created_at:       invoice_item[:created_at],
                     updated_at:       invoice_item[:updated_at]
                   )
