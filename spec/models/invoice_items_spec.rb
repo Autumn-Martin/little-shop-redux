9fc93bf65e4766bec198085e6eb40a15952e646b
Tim Fielder <fiel1139@gmail.com>
2018-08-20 17:02:12 -0600

    adds invoice_items_testing_spec for validations.

--- spec/models/invoice_items_spec.rb
+++ spec/models/invoice_items_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe InvoiceItems, type: :model do
     it { should validate_presence_of(:item_id)}
     it { should validate_presence_of(:invoice_id)}
     it { should belong_to(:quantity)}
-    it { should belong_to(:unit_price)}    
+    it { should belong_to(:unit_price)}
+      
   end
 end
