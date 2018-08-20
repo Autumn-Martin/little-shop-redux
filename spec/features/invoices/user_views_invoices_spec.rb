a4db73c7fdb3be38329f537826503cdbd24e2ce1
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-20 17:06:21 -0600

    add customer id to invoices

--- spec/features/invoices/user_views_invoices_spec.rb
+++ spec/features/invoices/user_views_invoices_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe 'Invoice Index Page' do
   before(:each) do
     @merchant_1 = Merchant.create(name: "Bilbo")
     @merchant_2 = Merchant.create(name: "Ikea")
-    @invoice_1 = Invoice.create(merchant_id: 1, status: "shipped")
-    @invoice_2 = Invoice.create(merchant_id: 2, status: "shipping")
+    @invoice_1 = Invoice.create(merchant_id: 1, customer_id: 1, status: "shipped")
+    @invoice_2 = Invoice.create(merchant_id: 2, customer_id: 2, status: "shipping")
   end
 
   context 'as a visitor to /invoices' do
