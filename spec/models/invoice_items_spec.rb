13f273c9adbf7fda05907e2a91a692f3d5f3b187
Tim Fielder <fiel1139@gmail.com>
2018-08-20 17:01:31 -0600

    working on styles for item.  Changed and added Invoice Items validations spec.

---
+++ spec/models/invoice_items_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe InvoiceItems, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of(:item_id)}
+    it { should validate_presence_of(:invoice_id)}
+    it { should belong_to(:quantity)}
+    it { should belong_to(:unit_price)}    
+  end
+end
