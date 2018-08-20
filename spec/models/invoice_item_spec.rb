f91af997e1faa6b74354b00980d916dfe161bf35
Tim Fielder <fiel1139@gmail.com>
2018-08-20 17:10:34 -0600

    adds testing and relationship to invoice and item models.

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,9 +1,9 @@
-RSpec.describe InvoiceItems, type: :model do
+RSpec.describe InvoiceItem, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:item_id)}
     it { should validate_presence_of(:invoice_id)}
     it { should belong_to(:quantity)}
     it { should belong_to(:unit_price)}
-      
+
   end
 end
