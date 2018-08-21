a91d56fec10720f1622e7c06a6c4d5128f727113
andrewetobin <andrewetobin@gmail.com>
2018-08-21 15:27:48 -0600

    invoice header

---
+++ spec/features/invoices/user_sees_invoice_show_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe "Invoice Show" do
+  before(:each) do
+    @merchant_1 = Merchant.create(name: "Bilbo")
+    @invoice_1 = Invoice.create(merchant_id: @merchant_1.id, status: 'shipped')
+    @invoice_2 = Invoice.create(merchant_id: @merchant_1.id, status: 'shipped')
+    @item_1 = @merchant_1.items.create(
+      title: 'Ligature',
+      description: 'Rovner ligature',
+      unit_price: 40,
+      )
+    @item_2 = @merchant_1.items.create(
+      title: 'Microphone',
+      description: 'Shure condensor mic',
+      unit_price: 300,
+      )
+  end
+  it "sees invoice header" do
+    visit "/invoices/#{@invoice_1.id}"
+
+    expect(page).to have_content("Invoice: #{@invoice_1.id} - #{@invoice_1.status}")
+  end
+
+
+
+
+
+end
