e3317080bf81d1225bd67d62b77f0766edbcce52
andrewetobin <andrewetobin@gmail.com>
2018-08-21 13:07:47 -0600

    feature spec for create merchant

--- spec/features/merchants/user_views_merchants_spec.rb
+++ spec/features/merchants/user_views_merchants_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe 'Merchant index page' do
       expect(page).to have_content(@merchant_1.name)
       expect(page).to have_content(@merchant_2.name)
     end
+    it 'can create a merchant' do
+      visit '/merchants'
+
+      click_link 'Create A New Merchant'
+
+      expect(current_path).to eq("/merchants/new")
+    end
+
   end
 end
