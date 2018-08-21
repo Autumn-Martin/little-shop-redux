87fba4ad600940a3bdb94db55a579e01dc7da113
andrewetobin <andrewetobin@gmail.com>
2018-08-21 15:03:45 -0600

    invoice index page

--- spec/features/invoices/user_views_invoices_spec.rb
+++ spec/features/invoices/user_views_invoices_spec.rb
@@ -2,15 +2,16 @@ RSpec.describe 'Invoice Index Page' do
   before(:each) do
     @merchant_1 = Merchant.create(name: "Bilbo")
     @merchant_2 = Merchant.create(name: "Ikea")
-    @invoice_1 = Invoice.create(merchant_id: 1, customer_id: 1, status: "shipped")
-    @invoice_2 = Invoice.create(merchant_id: 2, customer_id: 2, status: "shipping")
+    @invoice_1 = Invoice.create(merchant_id: 1, status: "shipped")
+    @invoice_2 = Invoice.create(merchant_id: 2, status: "shipping")
   end
-
   context 'as a visitor to /invoices' do
     it 'shows list of invoices' do
       visit '/invoices'
       expect(page).to have_content("#{@invoice_1.merchant_id}")
       expect(page).to have_content("#{@invoice_2.merchant_id}")
+      expect(page).to_not have_content(@invoice_1.status)
+      expect(page).to_not have_content(@invoice_2.status)
     end
   end
 end
