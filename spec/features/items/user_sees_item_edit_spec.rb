41f8dc02bda3ebb1d4789fc354fe14ff6f4b0fff
andrewetobin <andrewetobin@gmail.com>
2018-08-21 16:35:56 -0600

    it can edit an item

--- spec/features/items/user_sees_item_edit_spec.rb
+++ spec/features/items/user_sees_item_edit_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe 'User' do
     expect(page).to have_content(@item_1.merchant.name)
     # expect(page).to have_content(@item_1.unit_price)
   end
+  it "can click on cancel and go to item index" do
+    visit "/items/#{@item_1.id}/edit"
+
+    click_button "Cancel"
+
+    expect(current_path).to eq('/items')
+  end
+  it "can update an item" do
+    new_title = 'shirt'
+    visit "/items/#{@item_1.id}/edit"
+    fill_in 'item[title]', with: "#{new_title}"
+    click_button 'Update Item'
+
+    expect(current_path).to eq("/items/#{@item_1.id}")
+    expect(page).to have_content("#{new_title}")
+  end
 end
