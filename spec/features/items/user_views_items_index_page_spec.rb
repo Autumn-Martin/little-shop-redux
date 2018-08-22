a146e749672f0e77a2b8945367c0de89537b0477
Tim Fielder <fiel1139@gmail.com>
2018-08-21 23:09:39 -0600

    refactors item index feature spec.

--- spec/features/items/user_views_items_index_page_spec.rb
+++ spec/features/items/user_views_items_index_page_spec.rb
@@ -6,31 +6,27 @@ RSpec.describe 'Item index page' do
       @item_2 = @merchant_1.items.create(title: "shirt", description: "worn", unit_price: 12.13, image: 'https://www.kesslerandsons.com/wp-content/uploads/2015/04/yanagisawa-awo37-alto-logo.jpg')
   end
   context 'as a visitor' do
-    it 'shows list of items' do
+    before(:each) do
       visit '/items'
-
+    end
+    it 'shows list of items' do
       expect(page).to have_content(@item_1.title)
       expect(page).to have_content(@item_2.title)
     end
     it 'shows Dashboard link' do
-      visit '/items'
       expect(page).to have_link("Dashboard")
     end
     it 'shows create a new item button' do
-      visit '/items'
       expect(page).to have_link("Create A New Item")
     end
     it "shows pictures of item" do
-      visit '/items'
       expect(page).to have_css("img[src*='https://www.kesslerandsons.com/wp-content/uploads/2015/04/yanagisawa-awo37-alto-logo.jpg']")
     end
     it "shows price of item" do
-      visit '/items'
       expect(page).to have_content(@item_1.unit_price)
       expect(page).to have_content(@item_2.unit_price)
     end
     it "shows link to individual item" do
-      visit '/items'
       expect(page).to have_link(@item_2.title)
     end
   end
