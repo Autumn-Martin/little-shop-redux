9c173237d02980f25848cd5006316ae46b964895
Tim Fielder <fiel1139@gmail.com>
2018-08-21 19:11:56 -0600

    stopping point.

--- spec/features/items/user_views_dashboard_spec.rb
+++ spec/features/items/user_views_dashboard_spec.rb
@@ -1,1 +1,11 @@
+RSpec.describe 'User views item dashboard page' do
+  before(:each) do
 
+  end
+
+  context 'as a user on the dashboard page' do
+    before(:each) do
+      visit '/items/item-dashboard'
+    end
+  end
+end
